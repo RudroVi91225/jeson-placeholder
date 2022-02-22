4c306300b79c179abaad31f9703d12c57827d9eb
RudroVi91225 <rudrodu91225@gmail.com>
2022-02-23 00:49:26 +0600

    fourth

--- pra.js
+++ pra.js
@@ -10,6 +10,7 @@ function displayPosts(posts) {
     const newDiv = document.getElementById(`new-div`);
     for (const post of posts) {
         const div = document.createElement(`div`);
+        div.classList.add(`post`)
         div.innerHTML = `<h3> ${post.title} </h3>
         <p> ${post.body} </p>`;
         newDiv.appendChild(div);
